3b650401c8e872b057dafdd8c5f8abc921a78546
Adam Harbin <harbin.adam@gmail.com>
2022-09-17 16:52:03 -0500

    06

---
+++ 06/solve.clj
@@ -0,0 +1,31 @@
+(ns advent.2016.6
+  (:require [clojure.string :as s]))
+
+;; Solution 1
+
+(defn solve-column-1 [c]
+  (let [freqs (frequencies c)
+        highest-freq (apply max (vals freqs))
+        max-pair (filter (fn [[k v]] (= v highest-freq)) freqs)]
+    (first (first max-pair))))
+
+;; Solution 2
+
+(defn solve-column-2 [c]
+  (let [freqs (frequencies c)
+        highest-freq (apply min (vals freqs))
+        max-pair (filter (fn [[k v]] (= v highest-freq)) freqs)]
+    (first (first max-pair))))
+
+(defn solve [input]
+  (let [columns (apply map vector input)]
+    (apply str (map solve-column-2 columns))))
+
+(defn solve-file [input]
+  (-> input
+      slurp
+      s/split-lines
+      solve
+      prn))
+
+(solve-file "input.dat")
